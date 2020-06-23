077fe05e71c35785febcf912997292d40be4b6d5
Guzman Martinez <guzmanmusicoloco@gmail.com>
2020-06-23 12:33:01 +0100

    Implementado con FronEnd

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class ClienteRestController {
 	@Autowired
 	private IClienteService clienteService;
 	
+	@CrossOrigin ( origins ="http://localhost:4200")
 	@GetMapping("/clientes")
 	public List<Cliente> index(){
 		return clienteService.findAll();
