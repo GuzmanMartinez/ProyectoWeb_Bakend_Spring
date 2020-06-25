d5f6ebd5763ac3f238765a3dd7847329dbf014d1
Guzman Martinez <guzmanmusicoloco@gmail.com>
2020-06-25 20:21:30 +0100

    CRUD Creado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,20 @@
+
 INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Guzman','Martinez Santos','Guzman@email.net','2020-06-19');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Marta','Gimenez Muñoz','Marta@email.net','2016-12-29');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Antonio','Muñoz Martinez','Antonio@email.net','2005-06-01');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Jose','Ramirez Muñoz','Jose@email.net','2020-03-27');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Lucia','Peralta Martinez','Lucia@email.net','2014-05-19');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Flor','Tulipan Peralta','Flor@email.net','2000-12-05');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Marta','Gimenez Muñoz','Marta@email.net','2016-12-29');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Antonio','Muñoz Martinez','Antonio@email.net','2005-06-01');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Jose','Ramirez Muñoz','Jose@email.net','2020-03-27');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Lucia','Peralta Martinez','Lucia@email.net','2014-05-19');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Flor','Tulipan Peralta','Flor@email.net','2000-12-05');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Marta','Gimenez Muñoz','Marta@email.net','2016-12-29');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Antonio','Muñoz Martinez','Antonio@email.net','2005-06-01');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Jose','Ramirez Muñoz','Jose@email.net','2020-03-27');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Lucia','Peralta Martinez','Lucia@email.net','2014-05-19');
+INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Flor','Tulipan Peralta','Flor@email.net','2000-12-05');
 INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Marta','Gimenez Muñoz','Marta@email.net','2016-12-29');
 INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Antonio','Muñoz Martinez','Antonio@email.net','2005-06-01');
 INSERT INTO clientes (nombre,apellido,email,create_at)VALUES ('Jose','Ramirez Muñoz','Jose@email.net','2020-03-27');
